0606292794591154659ebf9fe0e3f26b676e76b4
collinsmezie <collinschimezieobinwa@gmail.com>
2022-11-15 17:55:00 +0400

    add new project

--- src/assets/data/data.js
+++ src/assets/data/data.js
@@ -35,7 +35,7 @@ export const projects = [
   {
     id: generate(),
     title: "Recipify",
-    description: "A website which enables users to browse a list of SpaceX tour rockets, make reservations and join missions.",
+    description: "The Rails Recipe app keeps track of all your recipes, ingredients, and inventory. It will allow you to save ingredients, keep track of what you have, create recipes, and generate a shopping list based on what you have and what you are missing from a recipe.",
     image: recipe,
     url: "https://recipeefy.herokuapp.com/",
     github: "https://github.com/collinsmezie/recipe-app",
