f2cbb480928e6c1b6856857738b32ee20beed6a0
collins <collinschimezieobinwa@gmail.com>
2023-09-21 11:29:19 +0100

    update portfolio

--- src/assets/data/data.js
+++ src/assets/data/data.js
@@ -11,8 +11,17 @@ import react from '../images/react.png';
 import redux from '../images/redux.png';
 import recipe from '../images/recipe.png';
 import budget from '../images/budget.png';
-import next from '../images/nextimages.png';
+import next from '../images/next.png';
 import blogapp from '../images/blogapp.png';
+import SpaceX from '../images/spaceTravellers.png';
+import bungalow from '../images/bungalow.png';
+import mongodb from '../images/mongodb.png';
+import express from '../images/express.png';
+import node from '../images/node.png';
+
+
+
+
 
 
 
@@ -20,7 +29,7 @@ export const projects = [
   {
     id: generate(),
     title: "Tech Bloggers Haven",
-    description: "An engaging web application designed for tech enthusiasts and developers to share their knowledge and insights with the world.",
+    description: "An engaging web app designed for tech enthusiasts and developers to share their knowledge and insights with the world.",
     image: blogapp,
     url: "https://jamstack-blog-nine.vercel.app/",
     github: "https://github.com/collinsmezie/jamstack-blog",
@@ -33,16 +42,27 @@ export const projects = [
     id: generate(),
     title: "Space Traveler's Hub",
     description: "A website which enables users to browse a list of SpaceX tour rockets, make reservations and join missions.",
-    image: "https://github.com/collinsmezie/space-travellers-react/blob/dev/public/spaceTravellers.png?raw=true",
+    image: SpaceX,
     url: "https://space-travellers-react.vercel.app",
     github: "https://github.com/collinsmezie/space-travellers-react",
     technologies: [react,javascript,redux,tailwind]
   },
 
+
+  {
+    id: generate(),
+    title: "Bungalow",
+    description: "Explore diverse menus, order online, and savor culinary delights and enjoy delicious cuisine delivered to your doorstep.",
+    image: bungalow,
+    url: "https://recipeefy.herokuapp.com/",
+    github: "https://github.com/collinsmezie/recipe-app",
+    technologies: [ mongodb, express, react, node ]
+  },
+
   {
     id: generate(),
     title: "Recipify",
-    description: "Recipify keeps track of all your recipes, ingredients, inventory and enables users create custom recipes, and generate shopping list.",
+    description: "Recipify tracks your recipes, ingredients, and inventory. Users can create custom recipes and generate shopping lists.",
     image: recipe,
     url: "https://recipeefy.herokuapp.com/",
     github: "https://github.com/collinsmezie/recipe-app",
@@ -52,7 +72,7 @@ export const projects = [
   {
     id: generate(),
     title: "TranXact",
-    description: "A simple web based application for tracking your budget.",
+    description: "A simple web based application for tracking your budget and monitoring your daily expenses.",
     image: budget,
     url: "https://tranxact-budget-app.herokuapp.com/",
     github: "https://github.com/collinsmezie/TranXact",
@@ -98,7 +118,7 @@ export const techSkills = [
   },
   {
     id: generate(),
-    skill: 'React.js/Next.js',
+    skill: 'MongoDB',
   },
   {
     id: generate(),
@@ -110,7 +130,7 @@ export const techSkills = [
   },
   {
     id: generate(),
-    skill: 'Jest/RSpec',
+    skill: 'ExpressJS',
   },
   {
     id: generate(),
@@ -118,11 +138,11 @@ export const techSkills = [
   },
   {
     id: generate(),
-    skill: 'MongoDB',
+    skill: 'Jest/RSpec',
   },
   {
     id: generate(),
-    skill: 'Agile Methodologies',
+    skill: 'React.js/Next.js',
   },
   {
     id: generate(),
@@ -130,14 +150,18 @@ export const techSkills = [
   },
   {
     id: generate(),
-    skill: 'Python',
+    skill: 'Agile Methodologies',
   },
   {
     id: generate(),
-    skill: 'Mobile/Responsive Design',
+    skill: 'NodeJs',
   },
   {
     id: generate(),
     skill: 'Algorithms and Data Structures',
+  },
+  {
+    id: generate(),
+    skill: 'Mobile/Responsive Design',
   },
 ]
--- src/components/project.js
+++ src/components/project.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { v4 as uuidv4 } from 'uuid';
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
 
-export default function Project({title, description, image, url, github ,technologies}) {
-  
+export default function Project({ title, description, image, url, github, technologies }) {
+
   return (
     <div className="flex flex-col max-w-sm shadow-lg" data-aos="fade-up" data-aos-delay="100">
       <img className="object-cover h-auto" src={image} alt={title} />
@@ -16,17 +16,20 @@ export default function Project({title, description, image, url, github ,technol
         <div className="py-2 project">
           <p className="text-base font-semibold text-white md:hover:animate-none">BUILT WITH:</p>
           {technologies.map((technology, index) =>
-          <img key={uuidv4()} src={technology} width={40} height={40}/>
+            <img key={uuidv4()} src={technology} width={40} height={40} />
           )}
         </div >
         <div className="view">
-          <button onClick={() => {window.location.href=url}} target="_blank" className="text-base font-semibold text-white md:hover:text-teal-300 md:hover:animate-none">
+          {/* <button onClick={() => {window.location.href=url}} target="_blank" className="text-base font-semibold text-white md:hover:text-teal-300 md:hover:animate-none">
             VIEW PROJECT &gt;
-          </button>
+          </button> */}
 
-          <a href={github} target="_blank" rel="noopener noreferrer" className="text-base font-semibold text-white md:hover:text-teal-300 md:hover:animate-none">
+          <button onClick={() => {window.location.href=url}} className="border px-8 mt-6 py-1 border-teal-300 bg-transparent hover:bg-teal-300 hover:border ease-in-out duration-700 transition-colors text-teal-300 hover:text-black font-bold w-max sticky z-[11]">VIEW PROJECT</button>
+
+
+          {/* <a href={github} target="_blank" rel="noopener noreferrer" className="text-base font-semibold text-white md:hover:text-teal-300 md:hover:animate-none">
             VIEW SOURCE &gt;
-          </a>
+          </a> */}
         </div>
       </div>
     </div>
